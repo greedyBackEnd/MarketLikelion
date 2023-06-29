563e4a2900d6759757ae6280e0481395321e2586
kdybe01 <kdybe01@gmail.com>
2023-06-29 17:34:52 +0900

    ✨ 스프링부트 초기 세팅

---
+++ src/main/java/com/example/marketLikelion/MarketLikelionApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketLikelion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketLikelionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketLikelionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/marketLikelion/MarketLikelionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketLikelion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketLikelionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
