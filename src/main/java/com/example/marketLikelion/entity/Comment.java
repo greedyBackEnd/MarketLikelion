6625ace3a4d6e063255d81bb07d9d4b9dcc45b3f
kdybe01 <kdybe01@gmail.com>
2023-07-04 23:59:47 +0900

    Feat : 댓글 엔티티 및 DTO 작성 - Comment.java   CommentRequestDto.java   CommentResponseDto.java

---
+++ src/main/java/com/example/marketLikelion/entity/Comment.java
@@ -0,0 +1,44 @@
+package com.example.marketLikelion.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder(toBuilder = true)
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "salesItemId")
+    private SalesItem salesItem;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String content;
+
+    private String reply;
+
+    public void updateComment(Comment commentUpdate) {
+        this.writer = commentUpdate.getWriter();
+        this.password = commentUpdate.getPassword();
+        this.content = commentUpdate.getContent();
+    }
+
+    public void registerReply(String reply) {
+        this.reply = reply;
+    }
+}
