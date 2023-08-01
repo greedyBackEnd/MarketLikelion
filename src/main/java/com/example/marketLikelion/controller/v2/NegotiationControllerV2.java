092ce1dac2f770933e29375b83628b53f9739b2b
kdybe01 <kdybe01@gmail.com>
2023-08-02 00:51:06 +0900

    feat : Negotiation 관련 로직이 로그인된 유저를 기반으로 동작할 수 있도록 메소드 추가 및 수정 - NegotiationControllerV2.java   NegotiationRepository.java   NegotiationService.java

---
+++ src/main/java/com/example/marketLikelion/controller/v2/NegotiationControllerV2.java
@@ -0,0 +1,81 @@
+package com.example.marketLikelion.controller.v2;
+
+import com.example.marketLikelion.dto.request.NegotiationRequestDto;
+import com.example.marketLikelion.dto.response.NegotiationResponseDto;
+import com.example.marketLikelion.jwt.JwtTokenUtils;
+import com.example.marketLikelion.service.NegotiationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v2/items/{itemId}/proposals")
+public class NegotiationControllerV2 {
+
+    private final NegotiationService negotiationService;
+    private final JwtTokenUtils jwtTokenUtils;
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> registerNegotiation(@PathVariable Long itemId,
+                                                                   @RequestBody NegotiationRequestDto requestDto) {
+        String username = jwtTokenUtils.getCurrentUsername();
+
+        negotiationService.registerNegotiation(itemId, requestDto, username);
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("message", "구매 제안이 등록되었습니다.");
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<NegotiationResponseDto>> getNegotiations(@PathVariable Long itemId,
+                                                                        @RequestParam(defaultValue = "0") int page,
+                                                                        @RequestParam(defaultValue = "10") int limit) {
+        String username = jwtTokenUtils.getCurrentUsername();
+
+        Pageable pageable = PageRequest.of(page, limit);
+        Page<NegotiationResponseDto> negotiations = negotiationService.getNegotiations(itemId, username, pageable);
+        return new ResponseEntity<>(negotiations, HttpStatus.OK);
+    }
+
+    @PutMapping("/{proposalId}")
+    public ResponseEntity<Map<String, String>> changeNegotiationStatus(@PathVariable Long itemId,
+                                                                       @PathVariable Long proposalId,
+                                                                       @RequestBody NegotiationRequestDto requestDto) {
+        String username = jwtTokenUtils.getCurrentUsername();
+
+        if (requestDto.getStatus() != null) {
+            negotiationService.changeNegotiationStatus(itemId, proposalId, requestDto, username);
+            Map<String, String> responseBody = new HashMap<>();
+            responseBody.put("message", "제안의 상태가 변경되었습니다.");
+            return new ResponseEntity<>(responseBody, HttpStatus.OK);
+        }
+
+        if (requestDto.getSuggestedPrice() != null) {
+            negotiationService.updateNegotiation(itemId, proposalId, requestDto, username);
+            Map<String, String> responseBody = new HashMap<>();
+            responseBody.put("message", "제안이 수정되었습니다.");
+            return new ResponseEntity<>(responseBody, HttpStatus.OK);
+        }
+
+        throw new IllegalArgumentException("잘못된 요청입니다.");
+    }
+
+    @DeleteMapping("/{proposalId}")
+    public ResponseEntity<Map<String, String>> deleteNegotiation(@PathVariable Long itemId,
+                                                                 @PathVariable Long proposalId) {
+        String username = jwtTokenUtils.getCurrentUsername();
+
+        negotiationService.deleteNegotiation(itemId, proposalId, username);
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("message", "제안을 삭제했습니다.");
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
+    }
+}
--- src/main/java/com/example/marketLikelion/service/NegotiationService.java
+++ src/main/java/com/example/marketLikelion/service/NegotiationService.java
@@ -4,9 +4,11 @@ import com.example.marketLikelion.dto.request.NegotiationRequestDto;
 import com.example.marketLikelion.dto.response.NegotiationResponseDto;
 import com.example.marketLikelion.entity.Negotiation;
 import com.example.marketLikelion.entity.SalesItem;
+import com.example.marketLikelion.entity.User;
 import com.example.marketLikelion.entity.enumStatus.NegotiationStatus;
 import com.example.marketLikelion.repository.NegotiationRepository;
 import com.example.marketLikelion.repository.SalesItemRepository;
+import com.example.marketLikelion.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +24,7 @@ public class NegotiationService {
 
     private final SalesItemRepository salesItemRepository;
     private final NegotiationRepository negotiationRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public void registerNegotiation(Long itemId, NegotiationRequestDto requestDto) {
@@ -30,6 +33,19 @@ public class NegotiationService {
         Negotiation negotiation = requestDto.toEntity().toBuilder()
                 .salesItem(salesItem)
                 .status(NegotiationStatus.SUGGESTED.getStatus())
+                .build();
+        negotiationRepository.save(negotiation);
+    }
+
+    @Transactional
+    public void registerNegotiation(Long itemId, NegotiationRequestDto requestDto, String username) {
+        SalesItem salesItem = salesItemRepository.findById(itemId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 아이템이 존재하지 않습니다. id=" + itemId));
+
+        Negotiation negotiation = requestDto.toEntity().toBuilder()
+                .salesItem(salesItem)
+                .status(NegotiationStatus.SUGGESTED.getStatus())
+                .user(userRepository.findByUsername(username).get())
                 .build();
         negotiationRepository.save(negotiation);
     }
@@ -47,6 +63,20 @@ public class NegotiationService {
         }
     }
 
+    public Page<NegotiationResponseDto> getNegotiations(Long itemId, String username, Pageable pageable) {
+        SalesItem salesItem = salesItemRepository.findById(itemId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 아이템이 존재하지 않습니다. id=" + itemId));
+        User user = userRepository.findByUsername(username).get();
+
+        if (salesItem.getUser().getUsername().equals(username)) {
+            return negotiationRepository.findBySalesItem(salesItem, pageable)
+                    .map(NegotiationResponseDto::of);
+        } else {
+            return negotiationRepository.findBySalesItemAndUser(salesItem, user, pageable)
+                    .map(NegotiationResponseDto::of);
+        }
+    }
+
     @Transactional
     public void updateNegotiation(Long itemId, Long proposalId, NegotiationRequestDto requestDto) {
         salesItemRepository.findById(itemId)
@@ -57,6 +87,22 @@ public class NegotiationService {
 
         if (!negotiation.getWriter().equals(requestDto.getWriter()) || !negotiation.getPassword().equals(requestDto.getPassword())) {
             throw new IllegalArgumentException("작성자나 비밀번호가 일치하지 않습니다.");
+        }
+
+        negotiation.updateSuggestedPrice(requestDto.getSuggestedPrice());
+        negotiationRepository.save(negotiation);
+    }
+
+    @Transactional
+    public void updateNegotiation(Long itemId, Long proposalId, NegotiationRequestDto requestDto, String username) {
+        salesItemRepository.findById(itemId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 아이템이 존재하지 않습니다. id=" + itemId));
+
+        Negotiation negotiation = negotiationRepository.findById(proposalId)
+                .orElseThrow(() -> new IllegalArgumentException("제안이 존재하지 않습니다. id=" + proposalId));
+
+        if (!negotiation.getUser().getUsername().equals(username)) {
+            throw new IllegalArgumentException("다른 유저의 제안입니다.");
         }
 
         negotiation.updateSuggestedPrice(requestDto.getSuggestedPrice());
@@ -79,6 +125,21 @@ public class NegotiationService {
     }
 
     @Transactional
+    public void deleteNegotiation(Long itemId, Long proposalId, String username) {
+        salesItemRepository.findById(itemId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 아이템이 존재하지 않습니다. id=" + itemId));
+
+        Negotiation negotiation = negotiationRepository.findById(proposalId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 제안이 존재하지 않습니다. id=" + proposalId));
+
+        if (!negotiation.getUser().getUsername().equals(username)) {
+            throw new IllegalArgumentException("다른 유저의 제안입니다.");
+        }
+
+        negotiationRepository.delete(negotiation);
+    }
+
+    @Transactional
     public void changeNegotiationStatus(Long itemId, Long proposalId, NegotiationRequestDto requestDto) {
         SalesItem salesItem = salesItemRepository.findById(itemId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 아이템이 존재하지 않습니다. id=" + itemId));
@@ -88,6 +149,34 @@ public class NegotiationService {
 
         if (!salesItem.getWriter().equals(requestDto.getWriter()) || !salesItem.getPassword().equals(requestDto.getPassword())) {
             throw new IllegalArgumentException("작성자 또는 비밀번호가 일치하지 않습니다.");
+        }
+
+        if (requestDto.getStatus().equals("확정") && !negotiation.getStatus().equals("수락")) {
+            throw new IllegalArgumentException("제안이 수락 상태에서만 확정 가능합니다.");
+        }
+
+        negotiation.updateStatus(requestDto.getStatus());
+
+        if (requestDto.getStatus().equals("확정")) {
+            salesItem.updateStatus("판매 완료");
+            List<Negotiation> otherNegotiations = negotiationRepository.findBySalesItemAndIdNot(salesItem, proposalId);
+            for (Negotiation otherNegotiation : otherNegotiations) {
+                otherNegotiation.updateStatus("거절");
+            }
+        }
+    }
+
+    @Transactional
+    public void changeNegotiationStatus(Long itemId, Long proposalId, NegotiationRequestDto requestDto, String username) {
+
+        SalesItem salesItem = salesItemRepository.findById(itemId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 아이템이 존재하지 않습니다. id=" + itemId));
+
+        Negotiation negotiation = negotiationRepository.findById(proposalId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 제안이 존재하지 않습니다. id=" + proposalId));
+
+        if (!salesItem.getUser().getUsername().equals(username)) {
+            throw new IllegalArgumentException("다른 유저가 작성한 글입니다.");
         }
 
         if (requestDto.getStatus().equals("확정") && !negotiation.getStatus().equals("수락")) {
