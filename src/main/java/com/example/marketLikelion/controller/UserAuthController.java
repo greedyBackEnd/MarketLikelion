de64aec3f5c5f6f21c3e45322aaa2814b9c63621
kdybe01 <kdybe01@gmail.com>
2023-08-01 13:55:28 +0900

    feat : 유저 관련 security config 및 service 로직 작성 - CustomUserDetails.java   JpaUserDetailsManager.java   UserAuthController.java   UserController.java   UserService.java   WebSecurityConfig.java

---
+++ src/main/java/com/example/marketLikelion/controller/UserAuthController.java
@@ -0,0 +1,64 @@
+package com.example.marketLikelion.controller;
+
+import com.example.marketLikelion.dto.request.UserLoginDto;
+import com.example.marketLikelion.dto.request.UserRegistrationDto;
+import com.example.marketLikelion.entity.CustomUserDetails;
+import com.example.marketLikelion.jwt.JwtTokenDto;
+import com.example.marketLikelion.jwt.JwtTokenUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class UserAuthController {
+
+    private final UserDetailsManager manager;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenUtils jwtTokenUtils;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserRegistrationDto dto) {
+        CustomUserDetails userDetails = CustomUserDetails.builder()
+                .username(dto.getUsername())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .phone(dto.getPhone())
+                .email(dto.getEmail())
+                .address(dto.getAddress())
+                .build();
+        try {
+            manager.createUser(userDetails);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getStatusCode());
+        }
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtTokenDto> login(@RequestBody UserLoginDto dto) {
+        UserDetails userDetails = manager.loadUserByUsername(dto.getUsername());
+        if (!passwordEncoder.matches(dto.getPassword(), userDetails.getPassword()))
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        JwtTokenDto response = new JwtTokenDto();
+        response.setToken(jwtTokenUtils.generateToken(userDetails));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/logout")
+    private ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return new ResponseEntity<>("logout success", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/marketLikelion/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.marketLikelion.service;
+
+import com.example.marketLikelion.dto.response.UserResponseDto;
+import com.example.marketLikelion.entity.User;
+import com.example.marketLikelion.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserResponseDto getUserProfile(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다." + username));
+        return UserResponseDto.of(user);
+    }
+
+}
